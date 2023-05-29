bb82b00cf786663cb7fc552ecfd55efd7c033814
Carlos González <carlos.rgn@gmail.com>
2023-05-28 18:20:30 -0600

    docker build use cache

--- projects/WebApi/build.sh
+++ projects/WebApi/build.sh
@@ -9,5 +9,12 @@ tag=$(date +%s)
 image=$repo_url:$tag
 
 aws ecr get-login-password --region $aws_region | docker login --username AWS --password-stdin $aws_account_id.dkr.ecr.$aws_region.amazonaws.com
-docker buildx build --platform linux/arm64 -t $image --push .
+docker buildx create --use --driver=docker-container
+docker buildx build \
+  --platform linux/arm64 \
+  -t $image \
+  --cache-to type=gha,mode=max,scope=web-api \
+  --cache-from type=gha,scope=web-api \
+  --push \
+  .
 echo $image
