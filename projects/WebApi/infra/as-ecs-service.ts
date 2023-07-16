8f43180b357d5fd50615c4acbf18bbfa5928156f
Carlos González <carlos.rgn@gmail.com>
2023-07-15 21:25:13 -0600

    using ecs

--- projects/WebApi/infra/as-ecs-service.ts
+++ projects/WebApi/infra/as-ecs-service.ts
@@ -2,59 +2,112 @@ import * as aws from '@pulumi/aws';
 import * as awsx from '@pulumi/awsx';
 import * as pulumi from '@pulumi/pulumi';
 
+const awsRegion = new pulumi.Config('aws').require('region');
+
 const infraStack = new pulumi.StackReference(
   `rhyek/bank-apis.infrastructure/${pulumi.getStack()}`
 );
 
-const ecsClusterName = infraStack.getOutput('ecsClusterName');
-const ecsSubnetId = infraStack.getOutput('ecsSubnetId');
+const vpcId = infraStack.getOutput('vpcId');
+const ecsClusterId = infraStack.getOutput('ecsClusterId');
+const appLogGroupName = infraStack.getOutput('appLogGroupName');
+const ecsSecurityGroupId = infraStack.getOutput('ecsSecurityGroupId');
 
-const ecsCluster = ecsClusterName.apply((n) =>
-  aws.ecs.getCluster({ clusterName: n })
-);
-
-const alb = new awsx.lb.ApplicationLoadBalancer('appLoadBalancer', {});
-
-pulumi.all([ecsCluster, ecsSubnetId]).apply(([ecsCluster, ecsSubnetId]) => {
-  const taskDefinition = new aws.ecs.TaskDefinition('appTaskDef', {
-    containerDefinitions: JSON.stringify([
-      {
-        name: 'my-app',
-        image: process.env.IMAGE_URI,
-        memory: 256,
-        portMappings: [
-          {
-            targetGroup: alb.defaultTargetGroup,
-          },
-        ],
-      },
-    ]),
-    family: 'appFamily',
-    networkMode: 'awsvpc',
-    requiresCompatibilities: ['EC2'],
-  });
-
-  const service = new aws.ecs.Service(
-    'appSvc',
+const albSecurityGroup = new aws.ec2.SecurityGroup('webApiAlbSecurityGroup', {
+  vpcId,
+  ingress: [
+    // alb listener port
     {
-      cluster: ecsCluster.id,
-      desiredCount: 1,
-      launchType: 'EC2',
-      taskDefinition: taskDefinition.arn,
-      waitForSteadyState: true,
-      networkConfiguration: {
-        subnets: [ecsSubnetId], // use a default subnet
+      protocol: 'tcp',
+      fromPort: 80,
+      toPort: 80,
+      cidrBlocks: ['0.0.0.0/0'],
+    },
+  ],
+  egress: [
+    {
+      protocol: '-1', // Represents all protocols.
+      fromPort: 0, // Represents all ports.
+      toPort: 0, // Represents all ports.
+      securityGroups: [ecsSecurityGroupId],
+    },
+  ],
+});
+
+new aws.ec2.SecurityGroupRule('webApiAlbToEcsSecurityGroupRule', {
+  securityGroupId: ecsSecurityGroupId,
+  type: 'ingress',
+  protocol: '-1',
+  fromPort: 0,
+  toPort: 0,
+  sourceSecurityGroupId: albSecurityGroup.id,
+});
+
+const alb = new awsx.lb.ApplicationLoadBalancer('webApiAppLoadBalancer', {
+  internal: false,
+  // securityGroups: [ecsSecurityGroupId],
+  listener: {
+    port: 80,
+  },
+  securityGroups: [albSecurityGroup.id],
+  defaultTargetGroup: {
+    targetType: 'instance',
+    healthCheck: {
+      enabled: true,
+      path: '/_health',
+      port: 'traffic-port',
+      protocol: 'HTTP',
+      interval: 5,
+      timeout: 2,
+      healthyThreshold: 2,
+      unhealthyThreshold: 3,
+    },
+  },
+});
+
+const taskDefinition = new aws.ecs.TaskDefinition('webApiAppTaskDef', {
+  containerDefinitions: pulumi.jsonStringify([
+    {
+      name: 'web-api',
+      image: process.env.IMAGE_URI,
+      memory: 256,
+      logConfiguration: {
+        logDriver: 'awslogs',
+        options: {
+          'awslogs-group': appLogGroupName,
+          'awslogs-region': awsRegion,
+          'awslogs-stream-prefix': 'ecs',
+        },
       },
-      // loadBalancers: [{
-      //     targetGroupArn: "<-group-arn>", // replace with target group ARN
-      //     containerName: "my-app",
-      //     containerPort: 8080,
-      // }],
-    }
-    // {
-    //   dependsOn: [ec2Instance],
-    // }
-  );
+      portMappings: [
+        {
+          // hostPort: 80, // use dynamic ports
+          containerPort: 3000,
+          protocol: 'tcp',
+        },
+      ],
+    },
+  ]),
+  family: 'appFamily',
+  // https://docs.aws.amazon.com/AmazonECS/latest/bestpracticesguide/networking-networkmode.html
+  // keeping it simple.
+  networkMode: 'bridge',
+  requiresCompatibilities: ['EC2'],
+});
+
+const service = new aws.ecs.Service('webApiAppSvc', {
+  cluster: ecsClusterId,
+  desiredCount: 1,
+  launchType: 'EC2',
+  taskDefinition: taskDefinition.arn,
+  waitForSteadyState: true,
+  loadBalancers: [
+    {
+      targetGroupArn: alb.defaultTargetGroup.arn,
+      containerName: 'web-api',
+      containerPort: 3000,
+    },
+  ],
 });
 
 export const appLoadBalancerUrl = alb.loadBalancer.dnsName;
