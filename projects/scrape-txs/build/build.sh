8d695c64bed038bad292ec969b91dc74ad1bd460
Carlos González <carlos.rgn@gmail.com>
2024-03-18 11:53:24 -0600

    docker buildx build

--- projects/scrape-txs/build/build.sh
+++ projects/scrape-txs/build/build.sh
@@ -3,10 +3,12 @@ set -e
 __dirname=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 cd $__dirname
 
-docker build -t scrape-txs:${IMAGE_TAG} -f Dockerfile $__dirname/../
-docker tag scrape-txs:${IMAGE_TAG} ${ECR_REPO_URL}:${IMAGE_TAG}
-
 host="${ECR_REPO_URL%%/*}"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $host
 
-docker push ${ECR_REPO_URL}:${IMAGE_TAG}
+docker buildx build \
+  --push -t ${ECR_REPO_URL}:${IMAGE_TAG} \
+  -f ./Dockerfile \
+  --cache-to type=gha,scope=scrape-txs,mode=max \
+  --cache-from type=gha,scope=scrape-txs \
+  $__dirname/../
