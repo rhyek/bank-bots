927fadc6605959fb23a2c9c327f86ccd35f0968b
Carlos González <carlos.rgn@gmail.com>
2024-04-01 11:57:56 -0600

    fix scripts

---
+++ projects/update-ynab/scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function_name="update-ynab-fn"
+
+if aws lambda get-function --function-name $function_name --region us-east-1 &> /dev/null; then
+  echo "Updating function"
+  aws lambda update-function-code --function-name $function_name \
+  	--zip-file fileb://$ZIPFILE
+else
+  echo "Creating function"
+  aws lambda create-function --function-name $function_name \
+  	--runtime provided.al2023 \
+  	--handler bootstrap \
+  	--architectures arm64 \
+  	--role $LAMBDA_ROLE_ARN \
+  	--zip-file fileb://$ZIPFILE
+fi
