3eb7d6e74b1ba6bd0d82e8425a31961ada2412de
Carlos González <carlos.rgn@gmail.com>
2024-04-01 12:13:01 -0600

    update-function-configuration

--- projects/update-ynab/scripts/deploy.sh
+++ projects/update-ynab/scripts/deploy.sh
@@ -2,20 +2,24 @@
 
 function_name="update-ynab-fn"
 
+envs="Variables={DATABASE_URL=$DATABASE_URL}"
+
 if aws lambda get-function --function-name $function_name --region us-east-1 &> /dev/null; then
   echo "Updating function"
-  aws lambda update-function-code --function-name $function_name \
-    --environment "Variables={DATABASE_URL=$DATABASE_URL" \
-  	--zip-file fileb://$ZIPFILE \
-    > /dev/null
+  aws lambda update-function-code \
+    --function-name $function_name \
+  	--zip-file fileb://$ZIPFILE
+  aws lambda update-function-configuration \
+    --function-name $function_name \
+    --environment $envs
 else
   echo "Creating function"
-  aws lambda create-function --function-name $function_name \
+  aws lambda create-function \
+    --function-name $function_name \
   	--runtime provided.al2023 \
   	--handler bootstrap \
   	--architectures arm64 \
   	--role $LAMBDA_ROLE_ARN \
-    --environment "Variables={DATABASE_URL=$DATABASE_URL" \
-  	--zip-file fileb://$ZIPFILE \
-    > /dev/null
+    --environment $envs
+  	--zip-file fileb://$ZIPFILE
 fi
