881eb635a5b0c55a41607e6e59f1a06b33fecacb
Carlos González <carlos.rgn@gmail.com>
2024-04-06 11:24:14 -0600

    lambda invoke policy

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -21,3 +21,29 @@ resource "aws_iam_role_policy_attachment" "lambda_role_policy_attachment" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_iam_policy" "lambda_invoke_policy" {
+  name        = "lambda_invoke_policy"
+  description = "Allows role to invoke any lambda function in the account"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "lambda:InvokeFunction"
+        ],
+        Resource = [
+          "arn:*:lambda:*:*:function:*",
+          "arn:*:lambda:*:*:function:*:*",
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "lambda_invoke_policy_attachment" {
+  name       = "lambda_invoke_policy_attachment"
+  roles      = [aws_iam_role.lambda_role.name]
+  policy_arn = aws_iam_policy.lambda_invoke_policy.arn
+}
