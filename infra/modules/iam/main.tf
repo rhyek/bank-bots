15afc6385d974929e468e7b5173294faf3a1b3cf
Carlos González <carlos.rgn@gmail.com>
2024-04-06 11:43:18 -0600

    aws_iam_role_policy_attachment

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -42,8 +42,7 @@ resource "aws_iam_policy" "lambda_invoke_policy" {
   })
 }
 
-resource "aws_iam_policy_attachment" "lambda_invoke_policy_attachment" {
-  name       = "lambda_invoke_policy_attachment"
-  roles      = [aws_iam_role.lambda_role.name]
+resource "aws_iam_role_policy_attachment" "lambda_invoke_policy_attachment" {
+  role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.lambda_invoke_policy.arn
 }
