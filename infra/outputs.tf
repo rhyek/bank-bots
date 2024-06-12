3f08c5ba187952bc447735dad764b2260872bedd
Carlos González <carlos.rgn@gmail.com>
2024-06-12 14:23:48 -0600

    re-add

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -6,6 +6,6 @@ output "iam_lambda_role_arn" {
   value = module.iam.iam_lambda_role_arn
 }
 
-# output "playwright_traces_s3_bucket_id" {
-#   value = module.s3.playwright_traces_s3_bucket_id
-# }
+output "playwright_traces_s3_bucket_id" {
+  value = module.s3.playwright_traces_s3_bucket_id
+}
