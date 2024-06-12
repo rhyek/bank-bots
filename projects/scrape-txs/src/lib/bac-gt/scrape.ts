8c32c5f0b9cd39f0b29804092976a94c6e34c654
Carlos González <carlos.rgn@gmail.com>
2024-06-12 17:28:53 -0600

    set host

--- projects/scrape-txs/src/lib/bac-gt/scrape.ts
+++ projects/scrape-txs/src/lib/bac-gt/scrape.ts
@@ -51,6 +51,7 @@ export async function bacGtScrape({
   });
   await page.route('https://www.baccredomatic.com/', async (route) => {
     const headers = Object.assign({}, route.request().headers(), {
+      Host: 'www.baccredomatic.com',
       Accept:
         'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
       'Accept-Encoding': 'gzip, deflate, br, zstd',
